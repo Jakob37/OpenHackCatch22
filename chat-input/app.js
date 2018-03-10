934a37cbe08be1efab4a87b5351c7c4ae883c2ee
danielhamngren <daniel@memoto.com>
2018-03-10 11:08:34 +0100

    Added express server

--- chat-input/app.js
+++ chat-input/app.js
@@ -1,4 +1,6 @@
 require('dotenv').config();
+var express = require('express');
+var app = express();
 
 var accountSid = process.env.twilioAccountSid;
 var authToken = process.env.twilioAuthToken;  
@@ -12,3 +14,14 @@ client.messages.create({
     from: '+46769439389' // From a valid Twilio number
 })
 .then((message) => console.log(message.sid));
+
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/message', function(req, res){
+    res.sendFile(__dirname + '/message.html');
+});
