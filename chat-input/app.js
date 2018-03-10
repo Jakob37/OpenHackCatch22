e719d0410325ef92cbbb337d629e1b8de8bd567c
danielhamngren <daniel@memoto.com>
2018-03-10 11:51:00 +0100

    switched to http post

--- chat-input/app.js
+++ chat-input/app.js
@@ -23,7 +23,7 @@ app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/message', function(req, res){
+app.post('/message', function(req, res){
 
     
     console.log(req.body.From);
