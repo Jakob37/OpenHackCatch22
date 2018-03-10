143d95ad7fbdb1f8561b17d7b44c570c9f106945
danielhamngren <daniel@memoto.com>
2018-03-10 23:08:50 +0100

    fix cross origin

--- chat-input/app.js
+++ chat-input/app.js
@@ -20,6 +20,15 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 app.use(express.json());       // to support JSON-encoded bodies
 app.use(express.urlencoded()); // to support URL-encoded bodies
 
+//This could potentally be unsecure and very dangerously...
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
 var accountSid = process.env.twilioAccountSid;
 var authToken = process.env.twilioAuthToken;
 
