5858eeacdd94765b080f5c39497d43d495e96624
danielhamngren <daniel@memoto.com>
2018-03-11 11:40:20 +0100

    fix express middleware

--- chat-input/app.js
+++ chat-input/app.js
@@ -17,8 +17,8 @@ app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 }));
-app.use(express.json());       // to support JSON-encoded bodies
-app.use(express.urlencoded()); // to support URL-encoded bodies
+//app.use(express.json());       // to support JSON-encoded bodies
+//app.use(express.urlencoded()); // to support URL-encoded bodies
 
 //This could potentally be unsecure and very dangerously...
 app.use(function(req, res, next) {
